68bccc18aa730fc6c744645fbea0439d543c51a8
gparusimov <gparusimov@luxoft.com>
2018-06-19 19:35:53 +0300

    Fixed new small issues

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     host: "localhost",
     port: 8545,
-    gas: 500000, // We know this is enough for this project, not necessarily your future projects
+    gas: 50000000, // We know this is enough for this project, not necessarily your future projects
     network_id: "*" // Match any network id
   }
 }
